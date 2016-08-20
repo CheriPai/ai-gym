ef53e87e57e3c14effa293ddb3500622090926ea
cheripai <datdo@rocketmail.com>
2016-08-19 21:36:03 -0700

    Keep track of scores of iterations

--- CartPole/train.py
+++ CartPole/train.py
@@ -1,25 +1,28 @@
 import gym
 import numpy as np
 
-num_epochs = 20
+num_samples = 40
+p = 0.2
 mu = np.random.rand(4)
 sigma = np.random.rand(4)
-theta = np.zeros(4)
+theta = np.zeros((100, 4))
+scores = np.zeros(100)
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
 
-    for epoch in range(num_epochs):
+    for i in range(num_samples):
         observation = env.reset()
-        theta = np.random.normal(mu, sigma, 4)
+        theta[i] = np.random.normal(mu, sigma, 4)
         for t in range(100):
             env.render()
-            action = np.dot(theta, observation)
+            action = np.dot(theta[i], observation)
             if action < 0:
                 action = 0
             else:
                 action = 1
             observation, reward, done, info = env.step(action)
             if done:
-                print("Episode finished after {} timesteps".format(t+1))
+                scores[i] = t+1
+                print("Episode finished after {} timesteps".format(scores[i]))
                 break
