bc86ef3318a221d6a1eb72191f7a2987b876f9dd
cheripai <datdo@rocketmail.com>
2016-09-04 14:56:22 -0700

    Get cross entropy reinforcement learning working

--- CartPole/train.py
+++ CartPole/train.py
@@ -1,7 +1,8 @@
 import gym
 import numpy as np
 
-num_samples = 40
+# num_samples = 18
+num_samples = 10
 iterations = 100
 p = 0.2
 mu = np.random.rand(4)
@@ -12,21 +13,25 @@ scores = np.zeros(iterations)
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
 
-    for i in range(num_samples):
-        observation = env.reset()
-        theta[i] = np.random.normal(mu, sigma, 4)
-        for t in range(iterations):
-            env.render()
-            action = np.dot(theta[i], observation)
-            if action < 0:
-                action = 0
-            else:
-                action = 1
-            observation, reward, done, info = env.step(action)
-            if done:
-                scores[i] = t+1
-                print("Episode finished after {} timesteps".format(scores[i]))
-                break
-        top_p = num_samples * p
+    while True:
+        for i in range(num_samples):
+            observation = env.reset()
+            theta[i] = np.random.normal(mu, sigma, 4)
+            for t in range(iterations):
+                env.render()
+                action = np.dot(theta[i], observation)
+                if action < 0:
+                    action = 0
+                else:
+                    action = 1
+                observation, reward, done, info = env.step(action)
+                if done:
+                    scores[i] = t+1
+                    print("Episode finished after {} timesteps".format(scores[i]))
+                    break
+        top_p = int(num_samples * p)
         top = scores.argsort()[-top_p:][::-1]
-        print(top)
+        mu = np.mean(theta[top], axis=0)
+        sigma = np.std(theta[top], axis=0)
+        print(mu, sigma)
+
