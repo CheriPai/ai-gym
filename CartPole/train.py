287598172079290a0ef04e59477113b4987943a3
cheripai <datdo@rocketmail.com>
2016-09-04 14:45:53 -0700

    Split best p iterations

--- CartPole/train.py
+++ CartPole/train.py
@@ -2,11 +2,12 @@ import gym
 import numpy as np
 
 num_samples = 40
+iterations = 100
 p = 0.2
 mu = np.random.rand(4)
 sigma = np.random.rand(4)
-theta = np.zeros((100, 4))
-scores = np.zeros(100)
+theta = np.zeros((iterations, 4))
+scores = np.zeros(iterations)
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
@@ -14,7 +15,7 @@ if __name__ == "__main__":
     for i in range(num_samples):
         observation = env.reset()
         theta[i] = np.random.normal(mu, sigma, 4)
-        for t in range(100):
+        for t in range(iterations):
             env.render()
             action = np.dot(theta[i], observation)
             if action < 0:
@@ -26,3 +27,6 @@ if __name__ == "__main__":
                 scores[i] = t+1
                 print("Episode finished after {} timesteps".format(scores[i]))
                 break
+        top_p = num_samples * p
+        top = scores.argsort()[-top_p:][::-1]
+        print(top)
