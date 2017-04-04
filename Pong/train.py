454f8ae6c5b008f01aaef170003a7fa51df10895
dat <datdo@rocketmail.com>
2017-04-04 14:54:02 -0700

    Use DQN

--- Pong/train.py
+++ Pong/train.py
@@ -1,132 +1,98 @@
+from __future__ import division
+import argparse
+
+from PIL import Image
 import numpy as np
-import pickle
 import gym
 
-# hyperparameters
-H = 200 # number of hidden layer neurons
-batch_size = 10 # every how many episodes to do a param update?
-learning_rate = 1e-4
-gamma = 0.99 # discount factor for reward
-decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
-resume = True # resume from previous checkpoint?
-render = False
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
+from keras.optimizers import Adam
+import keras.backend as K
 
-# model initialization
-D = 80 * 80 # input dimensionality: 80x80 grid
-if resume:
-  print("Resuming")
-  model = pickle.load(open('save.p', 'rb'))
-else:
-  model = {}
-  model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization
-  model['W2'] = np.random.randn(H) / np.sqrt(H)
-  
-grad_buffer = { k : np.zeros_like(v) for k,v in model.items() } # update buffers that add up gradients over a batch
-rmsprop_cache = { k : np.zeros_like(v) for k,v in model.items() } # rmsprop memory
+from rl.agents.dqn import DQNAgent
+from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+from rl.core import Processor
+from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
-def sigmoid(x): 
-  return 1.0 / (1.0 + np.exp(-x)) # sigmoid "squashing" function to interval [0,1]
 
-def prepro(I):
-  """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-  I = I[35:195] # crop
-  I = I[::2,::2,0] # downsample by factor of 2
-  I[I == 144] = 0 # erase background (background type 1)
-  I[I == 109] = 0 # erase background (background type 2)
-  I[I != 0] = 1 # everything else (paddles, ball) just set to 1
-  return I.astype(np.float).ravel()
+INPUT_SHAPE = (84, 84)
+WINDOW_LENGTH = 4
+STEPS = 10000000
 
-def discount_rewards(r):
-  """ take 1D float array of rewards and compute discounted reward """
-  discounted_r = np.zeros_like(r)
-  running_add = 0
-  for t in reversed(range(r.size)):
-    if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
-    running_add = running_add * gamma + r[t]
-    discounted_r[t] = running_add
-  return discounted_r
 
-def policy_forward(x):
-  h = np.dot(model['W1'], x)
-  h[h<0] = 0 # ReLU nonlinearity
-  logp = np.dot(model['W2'], h)
-  p = sigmoid(logp)
-  return p, h # return probability of taking action 2, and hidden state
+class AtariProcessor(Processor):
+    def process_observation(self, observation):
+        assert observation.ndim == 3  # (height, width, channel)
+        img = Image.fromarray(observation)
+        img = img.resize(INPUT_SHAPE).convert('L')  # resize and convert to grayscale
+        processed_observation = np.array(img)
+        assert processed_observation.shape == INPUT_SHAPE
+        return processed_observation.astype('uint8')  # saves storage in experience memory
 
-def policy_backward(eph, epdlogp):
-  """ backward pass. (eph is array of intermediate hidden states) """
-  dW2 = np.dot(eph.T, epdlogp).ravel()
-  dh = np.outer(epdlogp, model['W2'])
-  dh[eph <= 0] = 0 # backpro prelu
-  dW1 = np.dot(dh.T, epx)
-  return {'W1':dW1, 'W2':dW2}
+    def process_state_batch(self, batch):
+        # We could perform this processing step in `process_observation`. In this case, however,
+        # we would need to store a `float32` array instead, which is 4x more memory intensive than
+        # an `uint8` array. This matters if we store 1M observations.
+        processed_batch = batch.astype('float32') / 255.
+        return processed_batch
 
-env = gym.make("Pong-v0")
-observation = env.reset()
-prev_x = None # used in computing the difference frame
-xs,hs,dlogps,drs = [],[],[],[]
-running_reward = None
-reward_sum = 0
-episode_number = 0
-while True:
-  if render: env.render()
+    def process_reward(self, reward):
+        return np.clip(reward, -1., 1.)
 
-  # preprocess the observation, set input to network to be difference image
-  cur_x = prepro(observation)
-  x = cur_x - prev_x if prev_x is not None else np.zeros(D)
-  prev_x = cur_x
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', choices=['train', 'test'], default='train')
+parser.add_argument('--env-name', type=str, default='BreakoutDeterministic-v3')
+parser.add_argument('--weights', type=str, default=None)
+args = parser.parse_args()
 
-  # forward the policy network and sample an action from the returned probability
-  aprob, h = policy_forward(x)
-  action = 2 if np.random.uniform() < aprob else 3 # roll the dice!
+# Get the environment and extract the number of actions.
+env = gym.make(args.env_name)
+nb_actions = env.action_space.n
 
-  # record various intermediates (needed later for backprop)
-  xs.append(x) # observation
-  hs.append(h) # hidden state
-  y = 1 if action == 2 else 0 # a "fake label"
-  dlogps.append(y - aprob) # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+# Next, we build our model. We use the same model that was described by Mnih et al. (2015).
+input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
+model = Sequential()
+model.add(Permute((2, 3, 1), input_shape=input_shape))
+model.add(Convolution2D(32, 8, 8, subsample=(4, 4)))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 4, 4, subsample=(2, 2)))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 3, subsample=(1, 1)))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+print(model.summary())
 
-  # step the environment and get new measurements
-  observation, reward, done, info = env.step(action)
-  reward_sum += reward
+memory = SequentialMemory(limit=STEPS, window_length=WINDOW_LENGTH)
+processor = AtariProcessor()
 
-  drs.append(reward) # record reward (has to be done after we call step() to get reward for previous action)
+policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
+                              nb_steps=STEPS)
 
-  if done: # an episode finished
-    episode_number += 1
+dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
+               processor=processor, nb_steps_warmup=10000, gamma=.99, target_model_update=10000,
+               train_interval=4, delta_clip=1.)
+dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
-    # stack together all inputs, hidden states, action gradients, and rewards for this episode
-    epx = np.vstack(xs)
-    eph = np.vstack(hs)
-    epdlogp = np.vstack(dlogps)
-    epr = np.vstack(drs)
-    xs,hs,dlogps,drs = [],[],[],[] # reset array memory
+if args.mode == 'train':
+    weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
+    checkpoint_weights_filename = 'dqn_' + args.env_name + '_weights_{step}.h5f'
+    log_filename = 'dqn_{}_log.json'.format(args.env_name)
+    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=100000)]
+    callbacks += [FileLogger(log_filename, interval=100)]
+    dqn.fit(env, callbacks=callbacks, nb_steps=STEPS*2, log_interval=10000)
 
-    # compute the discounted reward backwards through time
-    discounted_epr = discount_rewards(epr)
-    # standardize the rewards to be unit normal (helps control the gradient estimator variance)
-    discounted_epr -= np.mean(discounted_epr)
-    discounted_epr /= np.std(discounted_epr)
+    dqn.save_weights(weights_filename, overwrite=True)
 
-    epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
-    grad = policy_backward(eph, epdlogp)
-    for k in model: grad_buffer[k] += grad[k] # accumulate grad over batch
-
-    # perform rmsprop parameter update every batch_size episodes
-    if episode_number % batch_size == 0:
-      for k,v in model.items():
-        g = grad_buffer[k] # gradient
-        rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
-        model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
-        grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
-
-    # boring book-keeping
-    running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-    print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
-    if episode_number % 100 == 0: pickle.dump(model, open('save.p', 'wb'))
-    reward_sum = 0
-    observation = env.reset() # reset env
-    prev_x = None
-
-  if reward != 0: # Pong has either +1 or -1 reward exactly when game ends.
-    print(('ep %d: game finished, reward: %f' % (episode_number, reward)) + ('' if reward == -1 else ' !!!!!!!!'))
+    dqn.test(env, nb_episodes=10, visualize=False)
+elif args.mode == 'test':
+    weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
+    if args.weights:
+        weights_filename = args.weights
+    dqn.load_weights(weights_filename)
+    dqn.test(env, nb_episodes=10, visualize=True)
